3e63548e2b23f251b31bde69f27282373a6ef0f9
Ann Dunska <anndunska@gmail.com>
2025-12-13 17:41:14 +0100

    Update ArgoCD bootstrap application and cleanup screenshots

--- terraform/argocd/main.tf
+++ terraform/argocd/main.tf
@@ -24,175 +24,34 @@ resource "helm_release" "argo" {
   ]
 }
 
-resource "kubernetes_manifest" "namespaces_appset" {
+resource "kubernetes_manifest" "argocd_gitops_repo" {
   manifest = {
     apiVersion = "argoproj.io/v1alpha1"
-    kind       = "ApplicationSet"
+    kind       = "Application"
     metadata = {
-      name      = "namespaces-appset"
-      namespace = var.argocd_namespace
+      name      = "gitops-root"
+      namespace = "argocd"
     }
     spec = {
-      generators = [{
-        git = {
-          repoURL    = var.app_repo_url
-          revision   = var.app_repo_branch
-          directories = [
-            { path = "namespaces/*" }
-          ]
-        }
-      }]
-      template = {
-        metadata = {
-          name      = "ns-{{path.basename}}"
-          namespace = var.argocd_namespace
-        }
-        spec = {
-          project = "default"
-          source = {
-            repoURL        = var.app_repo_url
-            targetRevision = var.app_repo_branch
-            path           = "{{path}}"
-            directory = { recurse = true }
-          }
-          destination = {
-            server    = "https://kubernetes.default.svc"
-            namespace = "{{path.basename}}"
-          }
-          syncPolicy = {
-            automated = { prune = true, selfHeal = true }
-            syncOptions = ["CreateNamespace=true"]
-          }
-          revisionHistoryLimit = 2
+      project = "default"
+
+      source = {
+        repoURL        = "https://github.com/Ember1995/goit-argo-9"
+        targetRevision = "main"
+        path           = "applications"
+      }
+
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "default"
+      }
+
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
         }
       }
     }
   }
-
-  depends_on = [helm_release.argo]
-}
-
-resource "kubernetes_manifest" "apps_appset" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "ApplicationSet"
-    metadata = {
-      name      = "apps-appset"
-      namespace = var.argocd_namespace  # infra-tools
-    }
-    spec = {
-      generators = [{
-        git = {
-          repoURL    = var.app_repo_url
-          revision   = var.app_repo_branch
-          directories = [
-            { path = "apps/*/*" } # apps/<namespace>/<app>
-          ]
-        }
-      }]
-
-      template = {
-        metadata = {
-          name      = "{{path.segments[2]}}"   # app name (mlflow)
-          namespace = var.argocd_namespace      # where ArgoCD lives
-        }
-
-        spec = {
-          project = "default"
-
-          destination = {
-            server    = "https://kubernetes.default.svc"
-            namespace = "{{path.segments[1]}}" # target namespace (application)
-          }
-
-          # Multi-source: chart из Helm repo + values из git
-          sources = [
-            {
-              repoURL        = "https://community-charts.github.io/helm-charts"
-              chart          = "{{path.segments[2]}}"    # имя чарта = имя папки (mlflow)
-              targetRevision = "0.1.10"                  # можно вынести в values.yaml позже
-              helm = {
-                valueFiles = [
-                  "$values/{{path}}/values.yaml"         # apps/<ns>/<app>/values.yaml
-                ]
-              }
-            },
-            {
-              repoURL        = var.app_repo_url
-              targetRevision = var.app_repo_branch
-              ref            = "values"
-            }
-          ]
-
-          syncPolicy = {
-            automated = { prune = true, selfHeal = true }
-            syncOptions = ["CreateNamespace=true"]
-          }
-
-          revisionHistoryLimit = 2
-        }
-      }
-    }
-  }
-
-  depends_on = [helm_release.argo]
-}
-
-resource "kubernetes_manifest" "monitoring_appset" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "ApplicationSet"
-    metadata = {
-      name      = "monitoring-appset"
-      namespace = var.argocd_namespace
-    }
-    spec = {
-      generators = [{
-        git = {
-          repoURL    = var.app_repo_url
-          revision   = var.app_repo_branch
-          directories = [
-            { path = "apps/monitoring/*" }
-          ]
-        }
-      }]
-      template = {
-        metadata = {
-          name      = "mon-{{path.basename}}"
-          namespace = var.argocd_namespace
-        }
-        spec = {
-          project = "default"
-          destination = {
-            server    = "https://kubernetes.default.svc"
-            namespace = "monitoring"
-          }
-          sources = [
-            {
-              repoURL        = "https://prometheus-community.github.io/helm-charts"
-              chart          = "{{path.basename}}"
-              targetRevision = "65.0.0"
-              helm = {
-                valueFiles = [
-                  "$values/{{path}}/values.yaml"
-                ]
-              }
-            },
-            {
-              repoURL        = var.app_repo_url
-              targetRevision = var.app_repo_branch
-              ref            = "values"
-            }
-          ]
-          syncPolicy = {
-            automated = { prune = true, selfHeal = true }
-            syncOptions = ["CreateNamespace=true"]
-          }
-          revisionHistoryLimit = 2
-        }
-      }
-    }
-  }
-
-  depends_on = [helm_release.argo]
 }
