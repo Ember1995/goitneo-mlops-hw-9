0b6c6250ae89bf9131e453435a00bc34e3877576
Ann Dunska <anndunska@gmail.com>
2025-12-13 18:17:38 +0100

    Fix ArgoCD install (CRDs) and bootstrap GitOps root app

--- terraform/argocd/main.tf
+++ terraform/argocd/main.tf
@@ -1,3 +1,34 @@
+# Namespace для ArgoCD
+resource "kubernetes_namespace" "argo" {
+  metadata {
+    name = var.argocd_namespace
+  }
+}
+
+# Встановлення ArgoCD через офіційний Helm-чарт
+resource "helm_release" "argo" {
+  name      = "argocd"
+  namespace = kubernetes_namespace.argo.metadata[0].name
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = var.argocd_chart_version
+
+  recreate_pods = true
+  replace       = true
+  set {
+  name  = "crds.install"
+  value = "false"
+}
+
+  values = [file("${path.module}/values/argocd-values.yaml")]
+
+  depends_on = [
+    kubernetes_namespace.argo
+  ]
+}
+
+# Bootstrap GitOps repo (ArgoCD will watch goit-argo-9/applications)
 resource "kubernetes_manifest" "argocd_gitops_repo" {
   manifest = {
     apiVersion = "argoproj.io/v1alpha1"
@@ -11,7 +42,7 @@ resource "kubernetes_manifest" "argocd_gitops_repo" {
 
       source = {
         repoURL        = "https://github.com/Ember1995/goit-argo-9"
-        targetRevision = "lesson-8-9"  # или "main"
+        targetRevision = "main"
         path           = "applications"
       }
 
@@ -34,3 +65,5 @@ resource "kubernetes_manifest" "argocd_gitops_repo" {
 
   depends_on = [helm_release.argo]
 }
+
+
